047ec9685b4dbbd8099ffe1dc1b002c394242bd5
ellenjaiany <131263420+ellenjaiany@users.noreply.github.com>
2023-05-30 14:29:01 -0300

    Add files via upload

---
+++ classes.js
@@ -0,0 +1,10 @@
+class Celular {
+    constructor(marca, modelo, ram, arm, proc, qtdDecam) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ram = ram;
+        this.arm = arm;
+        this.proc = proc;
+        this.qtdDecam = qtdDecam;
+    }
+}
---
+++ objetos.js
@@ -0,0 +1,49 @@
+let cel1 = new Celular (
+    "Samsung",
+    "A12",
+    "4 GB",
+    "64 GB",
+    "MediaTek Helio P35",
+    5,
+    
+)
+
+let cel2 = new Celular (
+    "Samsung",
+    "A10",
+    "2 GB",
+    "32 GB",
+    "Exynos 7884 Octa-Core",
+    3,
+    
+)
+
+let cel3 = new Celular (
+    "Motorola",
+    "Moto E22",
+    "4 GB",
+    "64 GB",
+    "Octa-Core",
+    3,
+    
+)
+
+let cel4 = new Celular (
+    "Apple",
+    "Iphone 11",
+    "4 GB",
+    "64 GB",
+    "A13 Bionic",
+    3,
+    
+)
+
+let cel5 = new Celular (
+    "Xiaomi",
+    "Redmi Note 9",
+    "4 GB",
+    "128 GB",
+    "MediaTek Helio G85",
+    5,
+    
+)
